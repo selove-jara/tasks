046356c640f42c8b25894ff8eed82a058c9ecf7b
Andrey Morozov <selove.jara@gmail.com>
2022-07-05 20:48:52 +0400

    коллекции

---
+++ src/task/lesson19/Task.java
@@ -0,0 +1,24 @@
+package task.lesson19;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Task {
+
+    public static void main(String[] args) {
+        List<Integer> list = new ArrayList<>(Arrays.asList(1, 3, 6, 8, 9, 13, 20));
+        List<Integer> result = removeEven(list);
+        System.out.println(result);
+    }
+
+    private static List<Integer> removeEven(List<Integer> list) {
+        List<Integer> result = new ArrayList<>();
+        for (Integer value : list) {
+            if (value % 2 != 0) {
+                result.add(value);
+            }
+        }
+        return result;
+    }
+}
